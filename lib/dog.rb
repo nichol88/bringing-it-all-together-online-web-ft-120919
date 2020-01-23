598c1bf18c216c5ed4edebd9cbe06bdf1c4f843f
Brian Nicholls <nichol88@gmail.com>
2020-01-23 01:04:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,7 +55,7 @@ class Dog
     self.new_from_db(DB[:conn].execute(sql, id)[0])
   end
 
-  def self.find_or_create_by
+  def self.find_or_create_by(name:, breed:)
     sql = <<-SQL
       SELECT * FROM dogs
       WHERE
